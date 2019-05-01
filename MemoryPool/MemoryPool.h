11596a4dcd0793eb7ac9656ac8a98e2b5212f5a3
LWL <lianwenliang@outlook.com>
2019-05-01 22:41:01 +0800

    20190501 update

---
+++ MemoryPool/MemoryPool.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef MemoryPool_H
+#define MemoryPool_H
+
+const int blockSize = 64;
+
+class ptr_memory_block
+{
+
+};
+
+class ptrPool
+{
+	
+};
+
+class MemoryPool 
+{
+
+};
+
+#endif // !MemoryPool_H
