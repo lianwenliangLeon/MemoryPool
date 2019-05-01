de177fb16731b1fdcc4dd487eb00c0e168c42120
LWL <lianwenliang@outlook.com>
2019-05-01 23:26:25 +0800

    20190501 2

--- MemoryPool/MemoryPool.h
+++ MemoryPool/MemoryPool.h
@@ -5,19 +5,44 @@
 
 const int blockSize = 64;
 
-class ptr_memory_block
+class MemoryBlock
 {
-
+private:
+	int backBlock;
+	int ptrIndex;
+	PtrMemoryBlock* ptr;
+public:
+	inline int GetBackBlock()
+	{
+		return backBlock;
+	}
+	inline int GetPtrIndex()
+	{
+		return ptrIndex;
+	}
+	inline PtrMemoryBlock* GetPtr()
+	{
+		return ptr;
+	}
 };
 
-class ptrPool
+class PtrMemoryBlock
 {
-	
+private:
+	MemoryBlock* blockPtr;
+	PtrMemoryBlock* pre;
+	PtrMemoryBlock* next;
+public:
+	inline PtrMemoryBlock* getNextPtr() { return next; }
+	inline PtrMemoryBlock* getPrePtr() { return pre; }
+	inline MemoryBlock* getMemBlo() { return blockPtr; }
 };
 
 class MemoryPool 
 {
-
+	PtrMemoryBlock* head;
+	int freeMemoryBlock;
+	void* memory;
 };
 
 #endif // !MemoryPool_H
