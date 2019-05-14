f7a3309138a7d4525791ac84a75ff08b7cc79137
LWL <lianwenliang@outlook.com>
2019-05-14 14:52:53 +0800

    Trans To laptop

--- MemoryPool/MemoryPool.cpp
+++ MemoryPool/MemoryPool.cpp
@@ -12,7 +12,7 @@ size_t check_algin_addr(void*& pBuf)
 }
 
 /*内存块大小对齐*/
-size_t check_algin_addr(size_t size)
+size_t check_align_block(size_t size)
 {
 	size_t align = size % MINUMITSIZE;
 
@@ -23,6 +23,7 @@ size_t check_algin_addr(size_t size)
 size_t check_align_size(size_t size)
 {
 	size = (size + SIZE_ALIGN - 1) / SIZE_ALIGN * SIZE_ALIGN;
+
 	return size;
 }
 
@@ -33,10 +34,119 @@ sBufSize：给定的内存大小
 */
 MemoryPool * MemoryPool::CreateMemoryPool(void* pBuf, size_t sBufSize)
 {
+	if (sizeof(MemoryPool) < sBufSize)
+		return NULL;
 	memset(pBuf, 0, sBufSize);
 	MemoryPool* MP = (MemoryPool*)pBuf;
 	size_t MemoryPoolSize = sizeof(MemoryPool);
 	MP->mem_map_pool_count = (sBufSize - MemoryPoolSize + MINUMITSIZE - 1) / MINUMITSIZE;//向上取整 块数
 	MP->mem_map_unit_count = (sBufSize - MemoryPoolSize + MINUMITSIZE - 1) / MINUMITSIZE;//向上取整 块数
-	MP->ptrMemoryMap = (MemoryBlock*)((char*)pBuf + MemoryPoolSize);
+
+	MP->ptrMemoryMap = (MemoryBlock*)((char*)pBuf + MemoryPoolSize);//可分配内存初始地址
+	if (!MP->ptrMemoryMap)
+		return NULL;
+	
+	MP->pfreeMemoryPool =(PtrMemoryBlock *)((char*)pBuf + MemoryPoolSize + sizeof(MemoryBlock) * MP->mem_map_unit_count);//指针起始地址
+	if (!MP->pfreeMemoryPool)
+		return NULL;
+
+	MP->memory = (char*)pBuf + MemoryPoolSize + sizeof(MemoryBlock) * MP->mem_map_unit_count + sizeof(PtrMemoryBlock) * MP->mem_map_pool_count;//总的存储空间地址为 指针所占空间 加上内存块所占空间 加上内存池所占空间
+	MP->Size = sBufSize - MemoryPoolSize - sizeof(MemoryBlock) * MP->mem_map_unit_count - sizeof(PtrMemoryBlock) * MP->mem_map_pool_count;
+	size_t align = check_algin_addr(MP->memory);
+	MP->Size = align;
+	MP->Size = check_align_block(MP->Size);
+	MP->mem_block_count = MP->Size / MINUMITSIZE;//块数
+
+	//链表
+	MP->pfreeMemoryPool = MP->create_list(MP->mem_map_pool_count);
+	//初始化 pfree_mem_chunk 双向循环链表
+	PtrMemoryBlock* tmp = MP -> front_pop();//取出一个指针 指向整个内存块
+	tmp->pre = tmp;
+	tmp->next = tmp;
+	tmp->blockPtr = NULL;
+	MP->mem_map_pool_count--;
+
+	//初始化 此时内存块不被分割 相当于只有一块很大的
+	MP->ptrMemoryMap[0].backBlock=MP->mem_block_count;                       
+	MP->ptrMemoryMap[0].ptr = tmp;
+	MP->ptrMemoryMap[MP->mem_block_count - 1].ptrIndex = 0;
+
+	tmp->blockPtr = MP->ptrMemoryMap;
+	MP->push_back(MP->pfreeMemory,tmp);
+	MP->free_mem_chunk_count = 1;
+	MP->mem_used_size = 0;
+	
+	return MP;
+}
+
+PtrMemoryBlock * MemoryPool::create_list(size_t count)//将链表区的单独PtrMemoryBlock链接为双向循环链表 并返回表头
+{
+	PtrMemoryBlock* pool = this->pfreeMemoryPool;
+	PtrMemoryBlock* head = NULL;
+	for (size_t i = 0; i < count; i++)
+	{
+		pool->pre = NULL;
+		pool->next = head;
+		if (head != NULL)
+		{
+			head->pre = pool;
+		}
+		head = pool;
+		pool++;
+	}
+	return head;
+}
+
+void* MemoryPool::GetMemory(size_t sMemorySize)
+{
+	sMemorySize = check_align_size(sMemorySize);//对齐内存
+	size_t index = 0;
+	PtrMemoryBlock* tmp = this->pfreeMemory;
+	for (index = 0; index < this->free_mem_chunk_count; index++)
+	{
+		if (tmp->blockPtr->backBlock * MINUMITSIZE >= sMemorySize)
+			break;// 找足够大的区域
+	}
+}
+
+PtrMemoryBlock* MemoryPool::front_pop()//首部元素出链表，并返回
+{
+	PtrMemoryBlock* pool = this->pfreeMemoryPool;
+
+	if (!this->pfreeMemoryPool)
+	{
+		return NULL;
+	}
+	PtrMemoryBlock* tmp = pool;
+	pool = tmp->next;
+	pool->pre = NULL;
+	return tmp;
+}
+
+bool MemoryPool::push_back(PtrMemoryBlock*& head, PtrMemoryBlock* element)//在链表尾部插入元素
+{
+	if (head == NULL)
+	{
+		head = element;
+		head->pre = element;
+		head->next = element;
+		return true;
+	}
+	head->pre->next = element;
+	element->pre = head->pre;
+	head->pre = element;
+	element->next = head;
+	return true;
+}
+
+bool MemoryPool::push_front(PtrMemoryBlock*& head, PtrMemoryBlock* element)//在首部插入元素
+{
+	element->pre = NULL;
+	element->next = head;
+	if (head != NULL)
+	{
+		head->pre = element;
+	}
+	head = element;
+	return true;
 }
--- MemoryPool/MemoryPool.h
+++ MemoryPool/MemoryPool.h
@@ -9,35 +9,19 @@ const size_t SIZE_ALIGN = MINUMITSIZE;
 
 class MemoryBlock
 {
-private:
+public:
 	int backBlock;//后面还有多少连续的空余内存卡=块
 	int ptrIndex;//指向这块内存块区域的指针的相对索引
 	PtrMemoryBlock* ptr;//指向 指向这块内存块的指针
-public:
-	inline int GetBackBlock(){
-		return backBlock;
-	}
-	inline int GetPtrIndex()
-	{
-		return ptrIndex;
-	}
-	inline PtrMemoryBlock* GetPtr()
-	{
-		return ptr;
-	}
 };
 
 class PtrMemoryBlock
 {
-private:
+public:
+	PtrMemoryBlock(PtrMemoryBlock * pool,size_t count);
 	MemoryBlock* blockPtr;//指向连续内存块首的指针
 	PtrMemoryBlock* pre;//前一个
 	PtrMemoryBlock* next;//后一个
-public:
-	inline PtrMemoryBlock* getNextPtr() { return next; }
-	inline PtrMemoryBlock* getPrePtr() { return pre; }
-	inline MemoryBlock* getMemBlo() { return blockPtr; }
-	PtrMemoryBlock(PtrMemoryBlock * pool,size_t count);
 };
 
 /*
@@ -47,23 +31,23 @@ class MemoryPool
 {
 private:
 	void* memory;//所属可分配的内存区域
-	size_t memorySize;
-	MemoryBlock* ptrMemoryMap;
-	PtrMemoryBlock* pfreeMemory;
-	PtrMemoryBlock* pfreeMemoryPool;
+	size_t Size;
+	MemoryBlock* ptrMemoryMap;//可分配内存地址
+	PtrMemoryBlock* pfreeMemory;//现在用的链表
+	PtrMemoryBlock* pfreeMemoryPool;//链表池 备用的
 	size_t mem_used_size;//已分配的内存块
-	size_t mem_map_pool_count; //记录链表单元缓冲池中剩余的单元的个数，个数为0时不能分配单元给pfree_mem_chunk
-	size_t free_mem_chunk_count;// 记录 pfree_mem_chunk链表中的单元个数 
+	size_t mem_map_pool_count; //记录链表池单元个数
+	size_t free_mem_chunk_count;// 记录 现在使用的链表的结点数
 	size_t mem_map_unit_count;
-	size_t mem_block_count;// 一个 mem_unit 大小为 MINUNITSIZE
+	size_t mem_block_count;// 记录内存块个数
 public:
 	static MemoryPool* CreateMemoryPool(void* pBuf, size_t PoolSize);
 	void* GetMemory(size_t MemorySize);
 	bool FreeMemory(void* memoryPtr);
 	PtrMemoryBlock* front_pop();
 	PtrMemoryBlock* create_list(size_t count);
-	bool push_back(PtrMemoryBlock* elment);
-	bool push_front(PtrMemoryBlock* element);
+	bool push_back(PtrMemoryBlock *& head,PtrMemoryBlock* elment);
+	bool push_front(PtrMemoryBlock *& head,PtrMemoryBlock* element);
 	bool deletePtr(PtrMemoryBlock* element);
 	size_t addrToIndex(void* addr);
 	size_t indexToAddr(size_t index);
